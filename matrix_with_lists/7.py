1de3a3c77244b38d89e85e042cd270ec47f4f195
Leina Pulgarin <leinapulgarin@MacBook-Pro-de-Macbook.local>
2020-08-12 11:38:44 -0500

    agregando matriz para pruebas

--- matrix_with_lists/7.py
+++ matrix_with_lists/7.py
@@ -1,25 +1,5 @@
-matrix = []
-M = int(input('Ingrese el número de filas de matriz: '))
-N = int(input('Ingrese el número de columnas de la matriz: '))
-count = 1
-for i in range(M):
-    matrix.append([])
-    for j in range(N):
-        matrix[i].append(count)
-        count += 1
-print()
-print()
-
-# Organizo la  matriz:  
-for row in matrix:
-    print('[', end=' ')
-    for element in row:
-        print('{:4}'.format(element), end="   ") 
-    print(']')
-    print()
-    print()
-    print()
-print()
+# Matriz para hacer las pruebas
+matrix = [[6,2,7,5],[9,7,8,1],[3,1,6,2]]
 
 # Eliminar una fila:
 def delete_row(matrix):
@@ -33,9 +13,9 @@ def delete_row(matrix):
 
 # Insertar una nueva fila:
 def insert_row(matrix):
-    new_row = [int(input('Ingrese un numero: ')) for i in range(N)]
+    new_row = [int(input('Ingrese un numero: ')) for i in range(len(matrix[0]))]
     A = int(input(f'Ingrese la posición de la fila que quiere cambiar, \
-    tenga encuenta que: posiciones >= 0 y < {M}: '))
+    tenga encuenta que: posiciones >= 0 y < {len(matrix)}: '))
     matrix_aux = [new_row if matrix[i] == matrix[A] else matrix[i] 
     for i in range(len(matrix))]
     matrix = matrix_aux
@@ -56,8 +36,9 @@ def delete_col(matrix):
 def insert_col(matrix):
     new_col = [int(input('Ingrese un numero: ')) for i in range(len(matrix))]
     B = int(input(f'Ingrese la posición de la columna que quiere cambiar, \
-    tenga encuenta que: posiciones >= 0 y < {N}: '))
-    matrix_aux = [[new_col[i] if matrix[i][B] == matrix[i][j] else matrix[i][j] for j in range(len(matrix[0]))] for i in range(len(matrix))]
+    tenga encuenta que: posiciones >= 0 y < {len(matrix[0])}: '))
+    matrix_aux = [[new_col[i] if matrix[i][B] == matrix[i][j] else matrix[i][j] \
+    for j in range(len(matrix[0]))] for i in range(len(matrix))]
     matrix = matrix_aux
     return matrix_aux
 #print(insert_col(matrix))
