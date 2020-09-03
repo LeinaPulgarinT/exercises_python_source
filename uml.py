29d492d2e6efc82518f61e0302fdaee29924a5a4
Leina Pulgarin <leinapulgarin@MacBook-Pro-de-Macbook.local>
2020-09-03 14:01:58 -0500

    corrigiendo los constructores de las clases

--- uml.py
+++ uml.py
@@ -7,7 +7,8 @@ class Node():
         self.node_right = None
 
 class Tree:
-    node = Node()
+    def __init__(self):
+        self.node = Node()
     
     def find(self, key):
         pass
@@ -17,7 +18,8 @@ class Tree:
 
 
 class Key:
-    node = None
+    def __init__(self, node):
+        self.node = node
 
     def is_equal(self, key):
         self.key = key
@@ -27,29 +29,38 @@ class Key:
         self.key = key
         
 class PersonKey(Key):
-    def __init__(self):
-        self.persona = None
+    def __init__(self, person, node):
+        super().__init__(node)
+        self.person_key = person
 
     def value():
         pass
 
 class StrKey(Key):
-    def __init__(self):
-        self.thing = ''
+    def __init__(self, thing, node):
+        super().__init__(node)
+        self.thing = thing
 
     def value():
         pass 
 
 
 class Value:
-    node = None
+    def __init__(self, node):
+        self.node = node
+
     def value():
         pass
 
 class PersonValue(Value):
-    def __init__(self):
-        self.person = None
+    def __init__(self, person, node):
+        super().__init__(node)
+        self.person_value = person
 
 class StringValue(Value):
-    def __init__(self):
-        self.thing = ''
+    def __init__(self, thing, node):
+        super().__init__(node)
+        self.thing = thing
+
+my_person = PersonValue('leina', 'a node')
+print(my_person.node)
